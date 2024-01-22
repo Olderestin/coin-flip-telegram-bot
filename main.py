7f2f238a4b4748f1a5579e682077b2693218ae13
Olderestin <sdordiuk@gmail.com>
2024-01-22 03:03:03 +0200

    Update handlers

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from telegram.ext import CommandHandler, ApplicationBuilder, MessageHandler, filters
 
-from handlers import start, flip, stats
+from handlers import start, flip, stats, about
 from config import BOT_TOKEN
 from models import create_user_table
 
@@ -13,8 +13,9 @@ def main() -> None:
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(MessageHandler(filters=filters.Regex('Flip'), callback=flip))
-    application.add_handler(MessageHandler(filters=filters.Regex('Stats'), callback=stats))
+    application.add_handler(MessageHandler(filters=filters.Regex(r'\bFlip\b'), callback=flip))
+    application.add_handler(MessageHandler(filters=filters.Regex(r'\bStats\b'), callback=stats))
+    application.add_handler(MessageHandler(filters=filters.Regex(r'\bAbout\b'), callback=about))
 
     
     application.run_polling()

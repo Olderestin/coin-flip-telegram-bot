8549bd6e553cf1c49d61850c52b273d4e8938522
Olderestin <sdordiuk@gmail.com>
2024-01-25 02:05:19 +0200

    Use Pathlib for DB_URL

--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+DATABASE_URL = 'storage/coin_flip_bot.db'
--- src/database.py
+++ src/database.py
@@ -1,6 +1,8 @@
 import sqlite3
+from pathlib import Path
+from config import DATABASE_URL
 
-DB_FILE = 'storage/coin_flip_bot.db'
+DB_FILE = Path(DATABASE_URL).resolve()
 
 def create_connection():
     return sqlite3.connect(DB_FILE)
